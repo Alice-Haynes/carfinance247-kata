979fa062973b14b006ca47fd69f55b25a2aedc52
interview <interview@CF247-DEV-L108.UK.CF247.NET>
2017-10-18 15:36:53 +0100

    initial commit

---
+++ Carfinance.Phoenix.Kata.Angular/app/list/list-controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('PhoenixKata').controller('ListController', ListController);
+
+    ListController.$inject = ['$http'];
+
+    function ListController($http) {
+        var vm = this;
+        
+        vm.table1 = {
+            name: "Mark Quinn",
+            phoneNo: "0782537383",
+            noOfDiners: 1,
+            tableNumber: 1,
+            bookingTime: new Date(),
+        };
+
+        function getBookings() {
+            $http.get("booking").then(function (response) {
+                vm.bookings = response.data;
+            });
+        }
+
+        getBookings();
+    }
+})();
